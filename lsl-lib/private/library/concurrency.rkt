01e8d56f7db7782dcd46af50d51ab10410943a67
Cameron Moy <mail@camoy.name>
2024-03-22 17:30:20 -0400

    concurrency gui init

--- lsl-lib/private/library/concurrency.rkt
+++ lsl-lib/private/library/concurrency.rkt
@@ -7,7 +7,8 @@
 (provide
  (contract-out
   [start start/c]
-  [start-debug start/c])
+  [start-debug start/c]
+  [start-gui start/c])
  (rename-out
   [process-macro process])
  Process
@@ -30,8 +31,7 @@
  receive-packet?
 
  action-state
- action-packets
- )
+ action-packets)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; require
@@ -45,7 +45,25 @@
          (only-in "core.rkt" define-struct)
          (only-in "contract.rkt" String Any)
          "../syntax/interface.rkt"
-         "../syntax/grammar.rkt")
+         "../syntax/grammar.rkt"
+
+         (except-in metapict identity text color table window @)
+         (prefix-in metapict: (only-in metapict text color))
+         racket/format
+         racket/string
+         racket/gui/easy
+         racket/gui/easy/operator
+         racket/list
+         racket/match
+         racket/math)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; constants
+
+(define R 1)
+(define AH 0.05)
+(define SIZE 800)
+(set-curve-pict-size SIZE SIZE)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; data
@@ -56,6 +74,11 @@
 (define-struct packet (from to msg))
 (define-struct send-packet (to msg))
 (define-struct receive-packet (from msg))
+
+(struct system (states channels))
+(struct move (system))
+(struct recv-move move (packet))
+(struct send-move move (packets))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; contracts
@@ -145,10 +168,18 @@
                             #:name "receive handler")
      #'(process n.c start.c recv.c)]))
 
-(define (start-debug scheduler processes)
-  (start scheduler processes #t))
+(define (start scheduler processes)
+  (start-help scheduler processes))
 
-(define (start scheduler processes [debug #f])
+(define (start-debug scheduler processes)
+  (start-help scheduler processes #:debug #t))
+
+(define (start-transcript scheduler processes)
+  (start-help scheduler processes #:transcript null))
+
+(define (start-help scheduler processes
+                    #:debug [debug #f]
+                    #:transcript [transcript #f])
   (define process-hash
     (for/hash ([p (in-list processes)])
       (values (process-name p) p)))
@@ -174,21 +205,32 @@
     (define possible (eligible-packets channels))
     (cond
       [(empty? possible)
-       (for/list ([(name state) (in-hash states)])
-         (^list name state))]
+       (define result
+         (for/list ([(name state) (in-hash states)])
+           (^list name state)))
+       (if transcript
+           (values result (reverse transcript))
+           result)]
       [else
-       (match-define (packet from to msg)
-         (scheduler possible))
+       (define pkt (scheduler possible))
+       (match-define (packet from to msg) pkt)
        (when debug
          (displayln (format ";;;; (packet #:from ~e #:to ~e #:msg ~e)" from to msg)))
        (define recv (process-recv (hash-ref process-hash to)))
        (define old-state (hash-ref states to))
-       (match-define (action next-state next-packets)
-         (recv old-state (make-receive-packet from msg)))
+       (define act (recv old-state (make-receive-packet from msg)))
+       (match-define (action next-state next-packets) act)
        (when debug
          (displayln (format ";;;; (state #:process ~e #:value ~e)" to next-state)))
-       (go (hash-set states to next-state)
-           (route* (pop-inbox channels from to) to next-packets))])))
+       (define states* (hash-set states to next-state))
+       (define channels* (route* (pop-inbox channels from to) to next-packets))
+       (when transcript
+         (set! transcript (cons (recv-move (system states channels) pkt) transcript))
+         (define out-pkts
+           (for/list ([pkt (in-list next-packets)])
+             (make-packet to (send-packet-to pkt) (send-packet-msg pkt))))
+         (set! transcript (cons (send-move (system states* channels*) out-pkts) transcript)))
+       (go states* channels*)])))
 
 (define (eligible-packets channels)
   (for*/list ([(to inbox) (in-hash channels)]
@@ -210,3 +252,129 @@
 (define (pop-inbox channels from to)
   (define inboxes (hash-ref channels to))
   (hash-set channels to (hash-update inboxes from rest)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; gui
+
+(define (start-gui scheduler processes)
+  (define-values (result transcript)
+    (start-transcript scheduler processes))
+  (define @steps (@ (make-history transcript)))
+  (define @tab (@ 'graph))
+
+  (define graph-view
+    (pict-canvas
+     #:min-size (list SIZE SIZE)
+     (@steps . ~> . history-current)
+     move->pict))
+
+  (define queues-view
+    (table
+     (list "To" "From" "Messages")
+     (@steps . ~> . (compose1 move->table history-current))))
+
+  (render
+   (window
+    (vpanel
+     (tabs
+      '(graph queues)
+      #:choice->label (compose1 string-titlecase symbol->string)
+      (λ (event _choices selection)
+        (match event
+          ['select (@tab . := . selection)]))
+      (observable-view
+       @tab
+       (match-lambda
+         ['graph graph-view]
+         ['queues queues-view])))
+     (hpanel
+      #:alignment '(center center)
+      (button "←"
+              (λ () (@steps . <~ . history-prev))
+              #:enabled? (@steps . ~> . history-has-prev?))
+      (button "→"
+              (λ () (@steps . <~ . history-next))
+              #:enabled? (@steps . ~> . history-has-next?))))))
+
+  result)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; history
+
+(struct history (context focus))
+
+(define (make-history xs)
+  (history '() xs))
+
+(define (history-next h)
+  (match-define (history ctx (cons fst rst)) h)
+  (history (cons fst ctx) rst))
+
+(define (history-prev h)
+  (match-define (history (cons fst rst) focus) h)
+  (history rst (cons fst focus)))
+
+(define (history-current h)
+  (first (history-focus h)))
+
+(define (history-has-next? h)
+  (cons? (rest (history-focus h))))
+
+(define (history-has-prev? h)
+  (cons? (history-context h)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; drawing helpers
+
+(define (move->table m)
+  (define sys (move-system m))
+  (define channels (system-channels sys))
+  (for*/vector ([(to inbox) (in-hash channels)]
+                [(from msgs) (in-hash inbox)])
+    (vector from to (string-join (map ~a msgs) ", "))))
+
+(define (move->pict m)
+  (define sys (move-system m))
+  (define ht (system->positions sys))
+  (draw
+   (draw-system ht sys)
+   (cond
+     [(recv-move? m) (draw-recv-move ht (recv-move-packet m))]
+     [(send-move? m) (draw-send-move ht (send-move-packets m))])))
+
+(define (draw-recv-move ht pkt)
+  (draw-packets ht (list pkt) "orange"))
+
+(define (draw-send-move ht pkts)
+  (define groups (group-by packet-to pkts))
+  (for/draw ([group (in-list groups)])
+    (draw-packets ht group "green")))
+
+(define (draw-system ht sys)
+  (for/draw ([(proc st) (in-hash (system-states sys))])
+    (define p (hash-ref ht proc))
+    (dot-label (format "~a ⟨~a⟩" proc st)
+               p
+               (if (> (pt-y p) 1/2) (top) (bot)))))
+
+(define (draw-packets ht pkts clr)
+  (for/draw ([pkt (in-list pkts)] [k (in-naturals 1)])
+    (draw-packet ht pkt (/ k (add1 (length pkts))) clr)))
+
+(define (draw-packet ht pkt α clr)
+  (match-define (packet from to msg) pkt)
+  (define src (hash-ref ht from))
+  (define dst (hash-ref ht to))
+  (define txt (~a (packet-msg pkt)))
+  (define dot (dot-label txt (med α src dst) (top)))
+  (let* ([result (draw-arrow (curve src .. dst)
+                             #:length AH
+                             #:color clr)]
+         [result (draw result dot)])
+    result))
+
+(define (system->positions sys)
+  (define states (system-states sys))
+  (for/hash ([(proc state) (in-hash states)]
+             [θ (in-range 0 360 (/ 360 (hash-count states)))])
+    (values proc (pt@d R θ))))
