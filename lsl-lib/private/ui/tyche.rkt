81cc745c30d49062000c1cba345692fb68d1287a
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-01-20 10:58:38 -0500

    Update button to run non-suite tests in file

--- lsl-lib/private/ui/tyche.rkt
+++ lsl-lib/private/ui/tyche.rkt
@@ -21,9 +21,12 @@
    "Launch Tyche"
    (pict->bitmap (standard-fish 32 16 #:color "salmon"))
    (λ (window)
+     (define interactions (send window get-interactions-text))
      (define editor (send window get-definitions-text))
      (define tmp (make-temporary-file))
-     (with-output-to-file tmp (lambda () (write-string (editor->string editor))) #:exists 'replace))
+     (with-output-to-file tmp (lambda () (write-string (editor->string editor))) #:exists 'replace)
+     (define output ((hash-ref (dynamic-require tmp 'run-tests) #f)))
+     (send interactions insert (format "~a" output)))
    #f))
 
 (define (editor->string ed)
